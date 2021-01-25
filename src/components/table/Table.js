1c18ba918e4c6b037e13389bd490e76fc20a4b2e
Andrei Chirkov <aachirkov@me.com>
2021-01-26 00:00:24 +0300

    Create resize el which can takes by mousedown and css for that

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -4,7 +4,15 @@ import {createTable} from '@/components/table/table.template';
 export class Table extends ExcelComponent {
   static className = 'excel__table'
 
+  constructor($root) {
+    super($root, {
+      listeners: ['mousedown']
+    })
+  }
+
   toHTML() {
     return createTable(20)
   }
+
+
 }
