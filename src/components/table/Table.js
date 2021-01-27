6f9e245128d7bc653794315973078af72826cf1c
Andrei Chirkov <aachirkov@me.com>
2021-01-27 22:04:12 +0300

    Refactor group select

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -2,7 +2,7 @@ import {ExcelComponent} from '@core/ExcelComponent';
 import {$} from '@core/dom';
 import {createTable} from '@/components/table/table.template';
 import {resizeHandler} from '@/components/table/table.resize';
-import {isCell, shouldResize} from '@/components/table/table.functions';
+import {isCell, matrix, shouldResize} from '@/components/table/table.functions';
 import {TableSelection} from '@/components/table/TableSelection';
 
 export class Table extends ExcelComponent {
@@ -35,23 +35,12 @@ export class Table extends ExcelComponent {
     } else if (isCell(event)) {
       // $target должен быть instanceof Dom
       const $target = $(event.target)
+
+      //групповое выделение если зажат shift
       if (event.shiftKey) {
-        //групповое выделение если зажат shift
-        const target = $target.id(true)
-        const current = this.selection.current.id(true)
-
-        //получить диапазон колонок и строк
-        const cols = range(current.col, target.col)
-        const rows = range(current.row, target.row)
-
-        //получаем все id из диапазона
-        const ids = cols.reduce((acc, col) => {
-          rows.forEach(row => acc.push(`${row}:${col}`))
-          return acc  //чтобы был виден на следующей итерации
-        }, [])
-
-        //массив выделенных элементов
-        const $cells = ids.map(id => this.$root.find(`[data-id="${id}"]`))
+        //получаем матрицу выделенных элементов
+        const $cells = matrix($target, this.selection.current)
+            .map(id => this.$root.find(`[data-id="${id}"]`))
         this.selection.selectGroup($cells)
       } else {
         this.selection.select($target)
--- src/components/table/table.functions.js
+++ src/components/table/table.functions.js
@@ -1,7 +1,24 @@
+import {range} from '@core/utils';
+
 export function shouldResize(event) {
   return event.target.dataset.resize
 }
 
 export function isCell(event) {
   return event.target.dataset.type === 'cell'
+}
+
+export function matrix($target, $current) {
+  const target = $target.id(true)
+  const current = $current.id(true)
+
+  //получить диапазон колонок и строк
+  const cols = range(current.col, target.col)
+  const rows = range(current.row, target.row)
+
+  //получаем все id из диапазона
+  return cols.reduce((acc, col) => {
+    rows.forEach(row => acc.push(`${row}:${col}`))
+    return acc  //чтобы был виден на следующей итерации
+  }, [])
 }
